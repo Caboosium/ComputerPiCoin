b50caba0a4bfd46ff0bf9de5044cdd33a61adb47
Caboosium <tydowntyty@yahoo.com>
2018-03-20 13:41:45 -0700

    Update upload.sh

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 FIXED_BRANCH=$(echo $BRANCH | sed 's/\//-/g')
-ARCHIVE=$REPO_NAME-$FIXED_BRANCH-$COMMIT.tar.bz2
+ARCHIVE="$REPO_NAME-$FIXED_BRANCH-$COMMIT.tar.bz2"
 echo "Creating archive $ARCHIVE"
-tar cfj $ARCHIVE dist
+tar cfj $ARCHIVE ./
 FILESIZE=$(stat -c%s "$ARCHIVE")
 echo "Finished archive (size $FILESIZE), starting Google Drive upload"
 ./bin/gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
