829319ae683ef93b4757edd4067b5319aa527541
Caboosium <tydowntyty@yahoo.com>
2018-03-20 14:01:51 -0700

    Update upload.sh

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 FIXED_BRANCH=$(echo $BRANCH | sed 's/\//-/g')
-ARCHIVE="Wallet.tar.bz2"
+ARCHIVE=Wallet.tar.bz2
 echo "Creating archive $ARCHIVE"
 tar cfj $ARCHIVE ./
-FILESIZE=$(stat -c%s "$ARCHIVE")
-echo "Finished archive (size $FILESIZE), starting Google Drive upload"
-./bin/gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
+echo "Finished archive, starting Google Drive upload"
+./bin/gdrive upload --list
+# --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
 echo "Finished Google Drive upload"
