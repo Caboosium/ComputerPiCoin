9b53c18d1a7561ace7b1bb88237bd9ae8ca58eaf
Caboosium <tydowntyty@yahoo.com>
2018-03-20 14:40:05 -0700

    Update upload.sh

--- bin/upload.sh
+++ bin/upload.sh
@@ -4,6 +4,5 @@ ARCHIVE=Wallet.tar.bz2
 echo "Creating archive $ARCHIVE"
 tar cfj $ARCHIVE ./
 echo "Finished archive, starting Google Drive upload"
-./bin/gdrive upload --list
-# --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
+./bin/gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
 echo "Finished Google Drive upload"
